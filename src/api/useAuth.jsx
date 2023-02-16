07b4d5b9d1dc7b87c090b794600d2dd60cfb9c12
thenotoriousgustav <rhezagustam@gmail.com>
2023-02-16 18:03:49 +0700

    fix env port again

--- src/api/useAuth.jsx
+++ src/api/useAuth.jsx
@@ -8,7 +8,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post('http://localhost:7708/login', {
+      .post('http://localhost:7526/login', {
         code,
       })
       .then((res) => {
@@ -26,7 +26,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return;
     const interval = setInterval(() => {
       axios
-        .post('http://localhost:7708/refresh', {
+        .post('http://localhost:7526/refresh', {
           refreshToken,
         })
         .then((res) => {
